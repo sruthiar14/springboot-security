065aca1754c389a3bef97a41fb785f1680b480a0
sruthiar14 <sruthi.ar@thoughtworks.com>
2023-04-20 19:36:52 +0530

    Revert "ar|sruthi removed method"
    This reverts commit 00c579873c0d128be1fa25e0f9499718407e7d7d.

--- src/main/java/com/myLearnings/security/repository/UserRepository.java
+++ src/main/java/com/myLearnings/security/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
     Optional<User> findByUsernameOrEmail(String name,String email);
 
     Boolean existsByUsername(String username);
